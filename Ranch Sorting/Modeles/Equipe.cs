1089f7d935ff66fe8af44e19866dbe4328928564
Mateo-lab <67467656+Mateo-lab@users.noreply.github.com>
2023-05-22 14:40:09 +0200

    Organisation fichier

---
+++ Ranch Sorting/Modeles/Equipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranch_Sorting.Modeles
+{
+    internal class Equipe
+    {
+    }
+}
