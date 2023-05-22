fff194845e32572a79845bb0b4dd1e67bdb23cf6
Mateo-lab <67467656+Mateo-lab@users.noreply.github.com>
2023-05-22 16:11:32 +0200

    Form Nouvelle Equipe

--- Ranch Sorting/Vue/NouvelleEquipe.Designer.cs
+++ Ranch Sorting/Vue/NouvelleEquipe.Designer.cs
@@ -28,12 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNomEquipe = new System.Windows.Forms.Label();
+            this.txtBoxNomPremierCavalier = new System.Windows.Forms.TextBox();
+            this.txtBoxNomPremierCheval = new System.Windows.Forms.TextBox();
+            this.txtBoxNomDeuxiemeCavalier = new System.Windows.Forms.TextBox();
+            this.txtBoxNomDeuxiemeCheval = new System.Windows.Forms.TextBox();
+            this.txtBoxNomEquipe = new System.Windows.Forms.TextBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(44, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom du premier cavalier :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(603, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom du deuxieme cheval :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(44, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nom du deuxieme cavalier :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(603, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(246, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nom du premier cheval :";
+            // 
+            // lblNomEquipe
+            // 
+            this.lblNomEquipe.AutoSize = true;
+            this.lblNomEquipe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomEquipe.Location = new System.Drawing.Point(298, 62);
+            this.lblNomEquipe.Name = "lblNomEquipe";
+            this.lblNomEquipe.Size = new System.Drawing.Size(278, 28);
+            this.lblNomEquipe.TabIndex = 4;
+            this.lblNomEquipe.Text = "Non de la nouvelle equipe : ";
+            this.lblNomEquipe.Click += new System.EventHandler(this.lblNomEquipe_Click);
+            // 
+            // txtBoxNomPremierCavalier
+            // 
+            this.txtBoxNomPremierCavalier.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNomPremierCavalier.Location = new System.Drawing.Point(342, 146);
+            this.txtBoxNomPremierCavalier.Name = "txtBoxNomPremierCavalier";
+            this.txtBoxNomPremierCavalier.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxNomPremierCavalier.TabIndex = 5;
+            // 
+            // txtBoxNomPremierCheval
+            // 
+            this.txtBoxNomPremierCheval.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNomPremierCheval.Location = new System.Drawing.Point(896, 146);
+            this.txtBoxNomPremierCheval.Name = "txtBoxNomPremierCheval";
+            this.txtBoxNomPremierCheval.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxNomPremierCheval.TabIndex = 6;
+            // 
+            // txtBoxNomDeuxiemeCavalier
+            // 
+            this.txtBoxNomDeuxiemeCavalier.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNomDeuxiemeCavalier.Location = new System.Drawing.Point(342, 244);
+            this.txtBoxNomDeuxiemeCavalier.Name = "txtBoxNomDeuxiemeCavalier";
+            this.txtBoxNomDeuxiemeCavalier.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxNomDeuxiemeCavalier.TabIndex = 7;
+            // 
+            // txtBoxNomDeuxiemeCheval
+            // 
+            this.txtBoxNomDeuxiemeCheval.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNomDeuxiemeCheval.Location = new System.Drawing.Point(896, 241);
+            this.txtBoxNomDeuxiemeCheval.Name = "txtBoxNomDeuxiemeCheval";
+            this.txtBoxNomDeuxiemeCheval.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxNomDeuxiemeCheval.TabIndex = 8;
+            // 
+            // txtBoxNomEquipe
+            // 
+            this.txtBoxNomEquipe.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNomEquipe.Location = new System.Drawing.Point(647, 59);
+            this.txtBoxNomEquipe.Name = "txtBoxNomEquipe";
+            this.txtBoxNomEquipe.Size = new System.Drawing.Size(192, 31);
+            this.txtBoxNomEquipe.TabIndex = 9;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.btnValider.Location = new System.Drawing.Point(193, 344);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(233, 64);
+            this.btnValider.TabIndex = 10;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.btnQuitter.Location = new System.Drawing.Point(723, 344);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(233, 64);
+            this.btnQuitter.TabIndex = 11;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            // 
+            // NouvelleEquipe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1157, 484);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.txtBoxNomEquipe);
+            this.Controls.Add(this.txtBoxNomDeuxiemeCheval);
+            this.Controls.Add(this.txtBoxNomDeuxiemeCavalier);
+            this.Controls.Add(this.txtBoxNomPremierCheval);
+            this.Controls.Add(this.txtBoxNomPremierCavalier);
+            this.Controls.Add(this.lblNomEquipe);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NouvelleEquipe";
             this.Text = "NouvelleEquipe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNomEquipe;
+        private System.Windows.Forms.TextBox txtBoxNomPremierCavalier;
+        private System.Windows.Forms.TextBox txtBoxNomPremierCheval;
+        private System.Windows.Forms.TextBox txtBoxNomDeuxiemeCavalier;
+        private System.Windows.Forms.TextBox txtBoxNomDeuxiemeCheval;
+        private System.Windows.Forms.TextBox txtBoxNomEquipe;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Button btnQuitter;
     }
 }
--- Ranch Sorting/Vue/NouvelleEquipe.cs
+++ Ranch Sorting/Vue/NouvelleEquipe.cs
@@ -16,5 +16,25 @@ namespace Ranch_Sorting.Vue
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNomEquipe_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
